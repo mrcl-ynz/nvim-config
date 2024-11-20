4d5d33daec65df34536b93b98739c4db7e17139d
Marcelo Yáñez <marcelo.araya1@mail.udp.cl>
2024-11-20 15:20:02 -0300

    feat: add plugin manager

--- init.lua
+++ init.lua
@@ -1,1 +1,23 @@
 require("config"):setup()
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+    local out = vim.system({ "git", "clone", "--filter=blob:none", "--branch=stable", "https://github.com/folke/lazy.nvim.git", lazypath }):wait()
+    if out.code ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out.stderr, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = {},
+    install = { colorscheme = { "default" } },
+    checker = { enabled = true },
+    change_detection = { notify = false },
+})
